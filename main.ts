58f1667eb4c284819a1003f9ce6c613ab4eeddc9
conradsoon <18553610+conradsoon@users.noreply.github.com>
2023-10-21 19:33:25 +0800

    Add Telegram bot

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import puppeteer, { Frame, Page } from "puppeteer";
+import { Context } from "telegraf";
+const { Telegraf, Scenes, session } = require("telegraf");
 require("dotenv").config();
 
 const selectByText = async (
@@ -63,146 +65,205 @@ const selectByText = async (
     }
   }
 };
-const bookSlot = async () => {
-  //   await Promise.all([
-  const browser = await puppeteer.launch({ headless: false });
-  const page: Page = await browser.newPage();
 
-  // Fill in the basic auth, replace 'username' and 'password' with your own credentials
-  const username = process.env.UTOWNFBS_USER as string;
-  const password = process.env.UTOWNFBS_PASS as string;
+const bookSlot = async (
+  date: string,
+  startTime: string,
+  endTime: string,
+  expectedAttendees: string,
+  usageType: string,
+  chargeGroup: string,
+  purpose: string
+): Promise<void> => {
+  let browser;
+  try {
+    browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const username = process.env.UTOWNFBS_USER;
+    const password = process.env.UTOWNFBS_PASS;
 
-  // console.log(`Username: //${username}//, Password: //${password}//`);
-  // Load the URL
-  await page.authenticate({ username, password });
-  //   await page.goto("https://utownfbs.nus.edu.sg/utown/apptop.aspx");
-  await Promise.all([
-    page.goto("https://utownfbs.nus.edu.sg/utown/apptop.aspx"),
-    page.waitForNavigation({ waitUntil: "networkidle0" }),
-  ]);
-  const frames = await page.frames();
-  // Iterate over each frame
-  for (const frame of frames) {
-    try {
-      // Attempt to select the element
-      await frame.waitForSelector('select[name="FacilityType$ctl02"]', {
-        timeout: 5000,
-      });
-      console.log("Select element found in frame with URL:", frame.url());
-      // Exit loop if element is found
-      break;
-    } catch (error) {
-      console.log("Select element not found in frame with URL:", frame.url());
+    if (!username || !password) {
+      throw new Error(
+        "Username or password is not set in environment variables"
+      );
     }
+
+    await page.authenticate({ username, password });
+    await Promise.all([
+      page.goto("https://utownfbs.nus.edu.sg/utown/apptop.aspx"),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+
+    const frames = await page.frames();
+    const targetFrame = frames.find(
+      (frame) =>
+        frame.url() ===
+        "https://utownfbs.nus.edu.sg/utown/modules/booking/search.aspx"
+    );
+
+    if (!targetFrame) {
+      throw new Error("Target frame not found");
+    }
+
+    await targetFrame.waitForSelector('select[name="FacilityType$ctl02"]', {
+      timeout: 5000,
+    });
+    await selectByText(
+      targetFrame,
+      'select[name="FacilityType$ctl02"]',
+      "(Seminar Room) - Facilities at Residential College 4"
+    );
+    await targetFrame.waitForTimeout(2000);
+
+    await selectByText(
+      targetFrame,
+      'select[name="Facility$ctl02"]',
+      "RC4 ORCA HUB (B1-45) (20 pax) (Residential College 4)"
+    );
+    await targetFrame.waitForTimeout(2000);
+
+    await targetFrame.evaluate((date) => {
+      const startDateField = document.getElementById(
+        "StartDate_ctl03"
+      ) as HTMLInputElement;
+      startDateField!.value = date;
+    }, date);
+
+    await targetFrame.evaluate((date) => {
+      const startDateField = document.getElementById(
+        "StartDate_ctl10"
+      ) as HTMLInputElement;
+      startDateField!.value = date;
+    }, date);
+
+    await targetFrame.click("input#btnViewAvailability");
+    await targetFrame.waitForTimeout(2000);
+    const divAvailable = await targetFrame.$(".divAvailable");
+
+    if (!divAvailable) {
+      throw new Error("No available slots found");
+    }
+
+    await divAvailable.click();
+    await targetFrame.waitForTimeout(10000);
+    const iframeElement = await targetFrame.$("#frmCreate");
+
+    if (!iframeElement) {
+      throw new Error("Iframe for booking creation not found");
+    }
+
+    const newFrame = await iframeElement.contentFrame();
+
+    if (!newFrame) {
+      throw new Error("Content frame not found in iframe");
+    }
+
+    await selectByText(newFrame, 'select[name="from$ctl02"]', startTime);
+    await newFrame.waitForTimeout(2000);
+    await selectByText(newFrame, 'select[name="to$ctl02"]', endTime);
+    await newFrame.waitForTimeout(2000);
+    await newFrame.type(
+      'input[name="ExpectedNoAttendees$ctl02"]',
+      expectedAttendees
+    );
+    await newFrame.waitForTimeout(2000);
+    await selectByText(newFrame, 'select[name="UsageType$ctl02"]', usageType);
+    await newFrame.waitForTimeout(2000);
+    await selectByText(
+      newFrame,
+      'select[name="ChargeGroup$ctl02"]',
+      chargeGroup
+    );
+    await newFrame.waitForTimeout(2000);
+    await newFrame.type('textarea[name="Purpose$ctl02"]', purpose);
+    await newFrame.waitForTimeout(2000);
+
+    await newFrame.click("#btnCreateBooking");
+    await newFrame.waitForTimeout(2000);
+
+    // Wait for 4 seconds
+    await newFrame!.waitForTimeout(4000);
+
+    // You can now check for the existence of the error message or proceed with the next steps
+    const errorMessageElement = await newFrame!.$("#labelMessage1");
+    if (errorMessageElement) {
+      const errorMessage = await newFrame!.$eval(
+        "#labelMessage1",
+        (el) => el.textContent
+      );
+      throw new Error(
+        errorMessage ? errorMessage : "Booking failed (null message)"
+      );
+    }
+
+    console.log("Booking has been made successfully");
+  } catch (error) {
+    console.error("Booking failed", error);
+    throw error;
+  } finally {
+    if (browser) await browser.close();
   }
-  // Obtain the frame where the element is found
-  const targetFrame = frames.find(
-    (frame) =>
-      frame.url() ===
-      "https://utownfbs.nus.edu.sg/utown/modules/booking/search.aspx"
-  );
-  // Extract options from the select element
-  const options = await targetFrame!.$$eval(
-    'select[name="FacilityType$ctl02"] > option',
-    (options) => {
-      return options.map((option) => option.value);
-    }
-  );
-  console.log(options);
-  // Use selectByText instead of the previous select function
-  await selectByText(
-    targetFrame!,
-    'select[name="FacilityType$ctl02"]',
-    "(Seminar Room) - Facilities at Residential College 4"
-  );
-
-  // Perform action on the select element
-  // await targetFrame!.select('select[name="FacilityType$ctl02"]', "optionValue");
-  // Select a facility by name
-  await targetFrame!.waitForTimeout(2000);
-  await selectByText(
-    targetFrame!,
-    'select[name="Facility$ctl02"]',
-    "RC4 ORCA HUB (B1-45) (20 pax) (Residential College 4)"
-  );
-  // await targetFrame!.select('select[name="FacilityType$ctl02"]', "optionValue");
-  //wait for 1000 0seconds
-  await targetFrame!.waitForTimeout(2000);
-  // Set the date
-  await targetFrame!.evaluate(() => {
-    const startDateField = <HTMLInputElement>(
-      document.getElementById("StartDate_ctl03")
-    );
-    startDateField.value = "22-Jul-2023"; // replace 'your-date-here' with the date you want
-  });
-
-  await targetFrame!.evaluate(() => {
-    const startDateField = <HTMLInputElement>(
-      document.getElementById("StartDate_ctl10")
-    );
-    startDateField.value = "22-Jul-2023"; // replace 'your-date-here' with the date you want
-  });
-
-  // Click the 'Search Availability' button
-  await targetFrame!.click("input#btnViewAvailability");
-
-  // You might need to wait for navigation if the click leads to a new page
-  // Note: This might not work as expected because `waitForNavigation` may not be fully compatible with frames. You might need to use a delay (`waitForTimeout`) or observe for some specific changes on the page.
-  // try {
-  // await targetFrame!.waitForNavigation({ timeout: 10000 });
-  // } catch (e) {
-  // console.log("Navigation timeout after 10 seconds");
-  // }
-  await targetFrame!.waitForTimeout(2000);
-  const divAvailable = await targetFrame!.$(".divAvailable");
-  await divAvailable!.click();
-
-  await targetFrame!.waitForTimeout(10000);
-  // Switching to the new iframe
-  const iframeElement = await targetFrame!.$("#frmCreate");
-  const newFrame = await iframeElement!.contentFrame();
-  //set newframe to be not null
-  //log newframe
-  const htmlContent = await newFrame!.content();
-  console.log(htmlContent);
-
-  // Selecting start time
-  await selectByText(newFrame!, 'select[name="from$ctl02"]', "12:00");
-  await newFrame!.waitForTimeout(2000);
-  await selectByText(newFrame!, 'select[name="to$ctl02"]', "14:30");
-  await newFrame!.waitForTimeout(2000);
-
-  // Filling in expected number of attendees
-  await newFrame!.type('input[name="ExpectedNoAttendees$ctl02"]', "2");
-  await newFrame!.waitForTimeout(2000);
-
-  await selectByText(
-    newFrame!,
-    'select[name="UsageType$ctl02"]',
-    "Student Activities"
-  );
-  await newFrame!.waitForTimeout(2000);
-
-  await selectByText(
-    newFrame!,
-    'select[name="ChargeGroup$ctl02"]',
-    "Official use related to academic duties"
-  );
-  await newFrame!.waitForTimeout(2000);
-
-  // Writing the purpose
-  await newFrame!.type('textarea[name="Purpose$ctl02"]', "Gym");
-  await newFrame!.waitForTimeout(2000);
-  await newFrame!.click("#btnCreateBooking");
-
-  // You'd typically have a 'Submit' or 'Next' button to click after filling the form, like:
-  // await newFrame!.click("input#submitButton");
-
-  // Remember to switch back to the main frame when you are done with the iframe
-
-  // Close the browser
-  await newFrame!.waitForTimeout(2000);
-  await browser.close();
 };
 
-bookSlot();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx: Context) =>
+  ctx.reply(
+    "Welcome! To make a booking, use the /book command followed by the booking details."
+  )
+);
+
+// @ts-ignore
+bot.command("book", async (ctx) => {
+  //if no message
+  // Check if the update contains a message and text
+  if (!ctx.message) {
+    return ctx.reply("This command can only be used with text messages.");
+  }
+
+  const parts = ctx.message.text.split(" ").slice(1);
+  if (parts.length < 4) {
+    return ctx.reply(
+      "Please provide all booking details. Usage: /book <date> <start time> <end time> <purpose>"
+    );
+  }
+
+  const [date, startTime, endTime, purpose, ...commentsParts] = parts;
+  const comments = commentsParts.join(" ") || undefined;
+
+  await ctx.reply("Booking in progress...");
+  try {
+    await bookSlot(
+      date,
+      startTime,
+      endTime,
+      "2",
+      "Student Activities",
+      "Official use related to academic duties",
+      purpose
+    );
+    await ctx.reply("Booking has been made successfully");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Booking failed", error);
+      await ctx.reply("Booking failed: " + error.message);
+    } else {
+      console.error("An unexpected error occurred:", error);
+      await ctx.reply("Booking failed due to an unexpected error.");
+    }
+  }
+});
+
+bot.launch();
+console.log("Bot is running...");
+
+// Usage
+// bookSlot(
+//   "25-Oct-2023",
+//   "12:00",
+//   "16:30",
+//   "2",
+//   "Student Activities",
+//   "Official use related to academic duties",
+//   "Gym",
+// );
